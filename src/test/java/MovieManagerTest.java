b06cf7730f25d14be6e5087521175a0a7beef8c8
Tatiana <ef.tatiana@mail.ru>
2024-06-14 16:27:14 +0300

    New tests added

--- src/test/java/MovieManagerTest.java
+++ src/test/java/MovieManagerTest.java
@@ -15,7 +15,9 @@ public class MovieManagerTest {
     @Test
     public void testFor1Movie() {
         MovieManager manager = new MovieManager();
+
         manager.addMovie("Movie 1");
+
         String[] expected = {"Movie 1"};
         String[] actual = manager.findAll();
         Assertions.assertArrayEquals(expected, actual);
@@ -24,20 +26,24 @@ public class MovieManagerTest {
     @Test
     public void testFor3Movies() {
         MovieManager manager = new MovieManager();
+
         manager.addMovie("Movie 1");
         manager.addMovie("Movie 2");
         manager.addMovie("Movie 3");
+
         String[] expected = {"Movie 1", "Movie 2", "Movie 3"};
         String[] actual = manager.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testForReverse() {
+    public void testForReverseUnderLimit5() {
         MovieManager manager = new MovieManager();
+
         manager.addMovie("Movie 1");
         manager.addMovie("Movie 2");
         manager.addMovie("Movie 3");
+
         String[] expected = {"Movie 3", "Movie 2", "Movie 1"};
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
@@ -46,11 +52,13 @@ public class MovieManagerTest {
     @Test
     public void testForLimit5() {
         MovieManager manager = new MovieManager();
+
         manager.addMovie("Movie 1");
         manager.addMovie("Movie 2");
         manager.addMovie("Movie 3");
         manager.addMovie("Movie 4");
         manager.addMovie("Movie 5");
+
         String[] expected = {"Movie 5", "Movie 4", "Movie 3", "Movie 2", "Movie 1"};
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
@@ -59,6 +67,7 @@ public class MovieManagerTest {
     @Test
     public void testForSettedLimit() {
         MovieManager manager = new MovieManager(7);
+
         manager.addMovie("Movie 1");
         manager.addMovie("Movie 2");
         manager.addMovie("Movie 3");
@@ -66,10 +75,104 @@ public class MovieManagerTest {
         manager.addMovie("Movie 5");
         manager.addMovie("Movie 6");
         manager.addMovie("Movie 7");
+
         String[] expected = {"Movie 1", "Movie 2", "Movie 3", "Movie 4", "Movie 5", "Movie 6", "Movie 7"};
         String[] actual = manager.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
+
+
+
+
+
+
+
+    @Test
+    public void testForReverseLimit5() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Movie 1");
+        manager.addMovie("Movie 2");
+        manager.addMovie("Movie 3");
+        manager.addMovie("Movie 4");
+        manager.addMovie("Movie 5");
+
+        String[] expected = {"Movie 5", "Movie 4","Movie 3", "Movie 2", "Movie 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testForReverseLimit5AndCountOfMoviesOverLimit() {
+        MovieManager manager = new MovieManager();
+
+        manager.addMovie("Movie 1");
+        manager.addMovie("Movie 2");
+        manager.addMovie("Movie 3");
+        manager.addMovie("Movie 4");
+        manager.addMovie("Movie 5");
+        manager.addMovie("Movie 6");
+
+        String[] expected = {"Movie 6", "Movie 5","Movie 4", "Movie 3", "Movie 2"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testForReverseSettedLimit8() {
+        MovieManager manager = new MovieManager(8);
+
+        manager.addMovie("Movie 1");
+        manager.addMovie("Movie 2");
+        manager.addMovie("Movie 3");
+        manager.addMovie("Movie 4");
+        manager.addMovie("Movie 5");
+        manager.addMovie("Movie 6");
+        manager.addMovie("Movie 7");
+        manager.addMovie("Movie 8");
+
+        String[] expected = {"Movie 8", "Movie 7","Movie 6","Movie 5", "Movie 4","Movie 3", "Movie 2", "Movie 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testForReverseSettedLimit8AndCountOfMoviesUnderLimit() {
+        MovieManager manager = new MovieManager(8);
+
+        manager.addMovie("Movie 1");
+        manager.addMovie("Movie 2");
+        manager.addMovie("Movie 3");
+        manager.addMovie("Movie 4");
+        manager.addMovie("Movie 5");
+        manager.addMovie("Movie 6");
+        manager.addMovie("Movie 7");
+
+        String[] expected = {"Movie 7","Movie 6","Movie 5", "Movie 4","Movie 3", "Movie 2", "Movie 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testForReverseSettedLimit8AndCountOfMoviesOverLimit() {
+        MovieManager manager = new MovieManager(8);
+
+        manager.addMovie("Movie 1");
+        manager.addMovie("Movie 2");
+        manager.addMovie("Movie 3");
+        manager.addMovie("Movie 4");
+        manager.addMovie("Movie 5");
+        manager.addMovie("Movie 6");
+        manager.addMovie("Movie 7");
+        manager.addMovie("Movie 8");
+        manager.addMovie("Movie 9");
+        manager.addMovie("Movie 10");
+        manager.addMovie("Movie 11");
+        manager.addMovie("Movie 12");
+
+        String[] expected = {"Movie 12", "Movie 11", "Movie 10", "Movie 9","Movie 8", "Movie 7", "Movie 6", "Movie 5" };
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
 
 
